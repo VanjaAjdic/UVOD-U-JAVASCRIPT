3a31bbf6480147cbffdeb8eb8ae4243edb849ab9
Vanja Ajdić <vajdic@smart.edu.rs>
2022-03-15 18:06:19 +0100

    radsacasa

---
+++ NAPREDNE/cas05/operatori.js
@@ -0,0 +1,26 @@
+// Spread operator
+
+function f(...niz){
+}
+
+f(4);
+
+f(5,6,7);
+
+function Suma(...brojevi){
+
+var Suma=0;
+
+brojevi.forEach(
+
+    function(value){
+
+        suma+= value;
+
+    }
+
+)
+
+return suma;
+
+}
